feac2f01d5078e59494f4a468786a4e4356dfa49
jobayer58 <jobayerafnan770@gmail.com>
2025-01-27 02:08:27 +0600

    update

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@
 const ErrorPage = () => {
     return (
         <div>
-            <h2 className="text-5xl text-center py-20">Page not Found</h2>
+            <h2 className="text-5xl text-center py-20">Sorry!! Page not Found</h2>
         </div>
     );
 };
